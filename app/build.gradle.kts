6cabea9402803c1ff86eaa7032bdf07e7491edfb
LittleSheep <hi@littlesheep.me>
2026-05-15 00:48:00 +0800

    :rocket: Launch 1.1+2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "dev.solsynth.cloudysky"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         manifestPlaceholders["appAuthRedirectScheme"] = "@string/app_auth_redirect_scheme"
         buildConfigField(
